0a676485bbfcffa7ad67f0b9757b7f76337def2c
RaymanFX <raymanfx@gmail.com>
2013-08-01 16:37:45 +0200

    display-legacy: use kernel headers

--- libgralloc/Android.mk
+++ libgralloc/Android.mk
@@ -20,7 +20,7 @@ include $(CLEAR_VARS)
 LOCAL_MODULE                  := gralloc.$(TARGET_BOARD_PLATFORM)
 LOCAL_MODULE_PATH             := $(TARGET_OUT_SHARED_LIBRARIES)/hw
 LOCAL_MODULE_TAGS             := optional
-LOCAL_C_INCLUDES              := $(common_includes)
+LOCAL_C_INCLUDES              := $(common_includes) $(kernel_includes)
 LOCAL_SHARED_LIBRARIES        := $(common_libs) libmemalloc libgenlock
 LOCAL_SHARED_LIBRARIES        += libqdutils libGLESv1_CM
 LOCAL_CFLAGS                  := $(common_flags) -DLOG_TAG=\"gralloc\"
@@ -31,9 +31,10 @@ include $(BUILD_SHARED_LIBRARY)
 include $(CLEAR_VARS)
 LOCAL_MODULE           := libmemalloc
 LOCAL_MODULE_TAGS      := optional
-LOCAL_C_INCLUDES       := $(common_includes)
+LOCAL_C_INCLUDES       := $(common_includes) $(kernel_includes)
 LOCAL_SHARED_LIBRARIES := $(common_libs) libgenlock libqdutils
 LOCAL_CFLAGS           := $(common_flags) -DLOG_TAG=\"memalloc\"
+LOCAL_ADDITIONAL_DEPENDENCIES := $(common_deps) $(kernel_deps)
 LOCAL_SRC_FILES        := alloc_controller.cpp
 ifeq ($(TARGET_USES_ION),true)
     LOCAL_SRC_FILES += ionalloc.cpp
